09dd0b4c35bfda212a15a40d0d15cf6fe51ed9ba
jinhee <rubjin@gmail.com>
2025-09-20 11:32:17 +0900

    [진희] 커밋추가

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="Message!!" />
+      <HelloWorld msg="Message!!" />
     </div>
   </header>
 
